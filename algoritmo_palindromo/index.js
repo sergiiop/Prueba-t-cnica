ead05d6cf5ea4a69e32d8971b66c380c9fe806f9
Sergio Pérez <sergio-luis98@hotmail.com>
2022-08-12 20:36:09 -0500

    agregando algoritmo

---
+++ algoritmo_palindromo/index.js
@@ -0,0 +1,54 @@
+const string = 'anulalalunapaisajemontanaguaamoraromacomidaluzazulsillagatobotellacamarayosoypalindromocasaverdebanderaventanacangrejolarutanosaportootropasonaturaliniciaracaestoseralodoodolaresdonasbarcosmarcieloaviontierrapaisbicicletaestonoespalindromojugarseverlasalrevesusandounradarenelojorejero'
+
+const letra = 'abcdefghijk'
+for(let i=0; i<letra.length; i++){
+  const foo='efg'
+  console.log(i)
+  if(i === 6){
+    i= i + (foo.length)
+  }
+}
+
+const comprobePalindrome = (str) => {
+  const reverseStr = str.split('').reverse().join('');
+  
+  if(reverseStr === str) return str
+  return false
+}
+
+const isPalindrome = (string) => {
+  /*const initialCompare = `${string[28]}${string[29]}${string[30]}`
+  const reversedCompare = initialCompare.split('').reverse().join('')
+  console.log(initialCompare)
+  console.log(reversedCompare)
+  console.log(string.includes(reversedCompare))
+  console.log(string.indexOf(reversedCompare))
+  console.log(string.indexOf(initialCompare))
+  const newString = string.slice(string.indexOf(initialCompare),string.indexOf(reversedCompare)+2 )
+  console.log(newString)
+  
+  */
+  const thePalindromes = []
+  
+  for(let i = 0; i<string.length;i++){
+    const initialCompare = `${string[i]}${string[i+1]}${string[i+2]}`
+  	const reversedCompare = initialCompare.split('').reverse().join('')
+    if(string.includes(reversedCompare)){
+      const newString = string.slice(string.indexOf(initialCompare),string.indexOf(reversedCompare)+3 )
+      if(newString.length > 3){
+      	const result = comprobePalindrome(newString)
+        if(result) {
+        	i= i+ newString.length
+          thePalindromes.push(result)
+        }
+      }
+    }
+  }
+  return {
+    palindromes_totales: thePalindromes.length,
+    palindromes: thePalindromes
+  }
+}
+
+
+isPalindrome(string)
